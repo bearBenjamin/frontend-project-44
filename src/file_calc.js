edc7c4e1b97e26ffaee863e5f6bc10baa8acba63
bearBenjamin <ilhakonev@yandex.ru>
2023-04-16 17:35:05 +0300

    add modified src/file_calc.js

--- src/file_calc.js
+++ src/file_calc.js
@@ -1,6 +1,6 @@
 import {
   showGreetings, showAnswerUser,
-  getUserName, getRandomNum,
+  getNameUser, getRandomNum,
   greetingsUser, roundQuestion, correctAnswer, exit,
   wrongAnswer, wrongAnswer1, congratulationsGame,
 } from './index.js';
@@ -8,52 +8,52 @@ import {
 // eslint-disable-next-line consistent-return
 const calculator = () => {
   showGreetings();
-  const userName = getUserName();
-  console.log(`${greetingsUser} ${userName}!`);
+  const nameUser = getNameUser();
+  console.log(`${greetingsUser} ${nameUser}!`);
   console.log('What is the result of the expression?');
   for (let i = 0; i < 3; i += 1) {
-    const numRandom = getRandomNum();
-    const numRandom1 = getRandomNum();
+    const numA = getRandomNum();
+    const numB = getRandomNum();
     const arrayRandOperation = ['+', '-', '*'];
     const randOperation = arrayRandOperation[Math.floor(Math.random() * arrayRandOperation.length)];
     const addition = randOperation === '+';
     const substraction = randOperation === '-';
     const multiplication = randOperation === '*';
     if (addition) {
-      console.log(`${roundQuestion}${numRandom} + ${numRandom1}`);
+      console.log(`${roundQuestion}${numA} + ${numB}`);
       const resultUser = +showAnswerUser();
-      const sum = numRandom + numRandom1;
+      const sum = numA + numB;
       if (resultUser === sum) {
         console.log(correctAnswer);
       } else {
-        console.log(`'${resultUser}' ${wrongAnswer}'${sum}'\n${wrongAnswer1}${userName}`);
+        console.log(`'${resultUser}' ${wrongAnswer}'${sum}'\n${wrongAnswer1}${nameUser}`);
         return exit;
       }
     }
     if (substraction) {
-      console.log(`${roundQuestion}${numRandom} - ${numRandom1}`);
+      console.log(`${roundQuestion}${numA} - ${numB}`);
       const resultUser = +showAnswerUser();
-      const sub = numRandom - numRandom1;
+      const sub = numA - numB;
       if (resultUser === sub) {
         console.log(correctAnswer);
       } else {
-        console.log(`'${resultUser}' ${wrongAnswer}'${sub}'\n${wrongAnswer1}${userName}`);
+        console.log(`'${resultUser}' ${wrongAnswer}'${sub}'\n${wrongAnswer1}${nameUser}`);
         return exit;
       }
     }
     if (multiplication) {
-      console.log(`${roundQuestion}${numRandom} * ${numRandom1}`);
+      console.log(`${roundQuestion}${numA} * ${numB}`);
       const resultUser = +showAnswerUser();
-      const multip = numRandom * numRandom1;
+      const multip = numA * numB;
       if (resultUser === multip) {
         console.log(correctAnswer);
       } else {
-        console.log(`'${resultUser}' ${wrongAnswer}'${multip}'\n${wrongAnswer1}${userName}`);
+        console.log(`'${resultUser}' ${wrongAnswer}'${multip}'\n${wrongAnswer1}${nameUser}`);
         return exit;
       }
     }
   }
-  console.log(`${congratulationsGame}, ${userName}!`);
+  console.log(`${congratulationsGame}, ${nameUser}!`);
   return exit;
 };
 
