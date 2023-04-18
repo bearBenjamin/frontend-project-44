9c5c025276771321ae6dd0622cae95de4fc4cb3f
bearBenjamin <ilhakonev@yandex.ru>
2023-04-18 14:46:04 +0300

    add modified src/index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,20 @@ export const showGreetings = () => {
   console.log('Welcome to the Brain Games!');
 };
 
+export const showGreetingsUser = (nameUser) => {
+  const greetingsUser = 'Hello, ';
+  console.log(`${greetingsUser}${nameUser}!`);
+};
+
+export const showRoundQuestion = (numA, operationRound, numB) => {
+  const roundQuestion = 'Question: ';
+  if (numB === undefined && operationRound === undefined) {
+    console.log(`${roundQuestion}${numA}`);
+  } else {
+    console.log(`${roundQuestion}${numA}${operationRound}${numB}`);
+  }
+};
+
 export const showAnswerUser = () => {
   const answerUser = question('Your answer: ');
   return answerUser;
@@ -14,8 +28,8 @@ export const showAnswerUser = () => {
 // function get;
 
 export const getNameUser = () => {
-  const userName = question('May I have your name? ');
-  return userName;
+  const nameUser = question('May I have your name? ');
+  return nameUser;
 };
 
 export const getRandomNum = () => {
@@ -27,6 +41,12 @@ export const getRandomNum = () => {
   return numRandom;
 };
 
+export const getAnswer = (numRandom) => {
+  const evenOrOdd = numRandom % 2 === 0;
+  const expectedResponse = evenOrOdd ? 'yes' : 'no';
+  return expectedResponse;
+};
+
 export const getRandomOperation = (arrayRandOperation = ['+', '-', '*']) => {
   const randOperation = arrayRandOperation[Math.floor(Math.random() * arrayRandOperation.length)];
   return randOperation;
@@ -34,16 +54,18 @@ export const getRandomOperation = (arrayRandOperation = ['+', '-', '*']) => {
 
 // list const;
 
-export const greetingsUser = 'Hello ,';
+// export const greetingsUser = 'Hello ,';
 
-export const roundQuestion = 'Question: ';
+// export const roundQuestion = 'Question: ';
 
-export const correctAnswer = 'Correct!';
+// export const correctAnswer = 'Correct!';
+
+export const exit = 'exit';
 
 export const wrongAnswer = 'is wrong answer ;(. Correct answer was ';
 
 export const wrongAnswer1 = 'Let\'s try again, ';
 
-export const congratulationsGame = 'Congratulations';
+export const correctAnswer = 'Correct!';
 
-export const exit = 'exit';
+export const congratulationsGame = 'Congratulations';
