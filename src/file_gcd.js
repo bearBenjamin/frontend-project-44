17af5e33505a38150c647ba0722a425e80409f6f
bearBenjamin <ilhakonev@yandex.ru>
2023-04-19 10:29:22 +0300

    add modified src/file_gcd.js

--- src/file_gcd.js
+++ src/file_gcd.js
@@ -1,47 +1,43 @@
 import {
-  showGreetings, showAnswerUser,
+  showGreetings, showAnswerUser, showGreetingsUser, showRoundQuestion,
   getNameUser, getRandomNum,
-  greetingsUser, roundQuestion, correctAnswer, exit,
+  correctAnswer, exit,
   wrongAnswer, wrongAnswer1, congratulationsGame,
 } from './index.js';
 
 const gcd = () => {
+  console.log('brain-gcd\n');
   showGreetings();
   const nameUser = getNameUser();
-  console.log(`${greetingsUser}${nameUser}!`);
+  showGreetingsUser();
   console.log('Find the greatest common divisor of given numbers.');
   let gcdResult;
   for (let i = 0; i < 3; i += 1) {
     let numA = getRandomNum();
     let numB = getRandomNum();
-    console.log(`${roundQuestion}${numA}${' '}${numB}`);
+    const operationRound = ' ';
+    showRoundQuestion(numA, operationRound, numB);
+    // console.log(`${roundQuestion}${numA}${}${numB}`);
     const answerUser = +showAnswerUser();
     if (numA === numB) {
       gcdResult = numA;
-      const correct = gcdResult === answerUser;
-      if (correct) {
-        console.log(correctAnswer);
-      } else {
-        console.log(`${wrongAnswer}${gcdResult}\n${wrongAnswer1}${nameUser}`);
-        return exit;
-      }
+    } else {
+      do {
+        if (numA > numB) {
+          numA -= numB;
+        } else {
+          numB -= numA;
+        }
+      } while (numA !== numB);
+      gcdResult = numA;
     }
-    do {
-      if (numA > numB) {
-        numA -= numB;
-      } else {
-        numB -= numA;
-      }
-    } while (numA !== numB);
-    gcdResult = numA;
-    const correct = gcdResult === answerUser;
-    if (correct) {
+    if (gcdResult === answerUser) {
       console.log(correctAnswer);
     } else {
       console.log(`${wrongAnswer}${gcdResult}\n${wrongAnswer1}${nameUser}`);
       return exit;
     }
-  }
+}
   console.log(`${congratulationsGame}${' '}${nameUser}!`);
   return exit;
 };
