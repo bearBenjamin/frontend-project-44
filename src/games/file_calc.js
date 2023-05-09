3587ca7e6cf45c8b862182ac4f7599928f803177
bearBenjamin <ilhakonev@yandex.ru>
2023-05-09 23:58:11 +0300

    modified src/games/file_calc.js

--- src/games/file_calc.js
+++ src/games/file_calc.js
@@ -7,12 +7,25 @@ const getRandomOperation = (arrayRandOperation = ['+', '-', '*']) => {
   const randOperation = arrayRandOperation[Math.floor(Math.random() * arrayRandOperation.length)];
   return randOperation;
 };
+const showRoundQuestionCalc = (numA, numB, randOperation) => {
+  let questionRound;
+  if (randOperation === '+') {
+    questionRound = `${'Question: '}${numA} + ${numB}`;
+  }
+  if (randOperation === '-') {
+    questionRound = `${'Question: '}${numA} - ${numB}`;
+  }
+  if (randOperation === '*') {
+    questionRound = `${'Question: '}${numA} * ${numB}`;
+  }
+  return questionRound;
+};
 
 const generateRound = () => {
   const numA = getRandomNum();
   const numB = getRandomNum();
   const randOper = getRandomOperation();
-  const questionRound = `${'Question: '}${numA}${randOper}${numB}`;
+  const questionRound = showRoundQuestionCalc(numA, numB, randOper);
   const add = randOper === '+' ? numA + numB : null;
   const sub = randOper === '-' ? numA - numB : null;
   const multip = randOper === '*' ? numA * numB : null;
